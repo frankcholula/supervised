b7fea1bf85826678772a401cec16f4ac80046a17
Artem Karamyshev <artem.karamyshev@jetbrains.com>
2025-02-01 11:58:26 +0000

    update style

--- supervised/scrape.py
+++ supervised/scrape.py
@@ -1,3 +1,4 @@
+import random
 import streamlit as st
 from scholarly import scholarly
 
@@ -30,49 +31,40 @@ professors = [
 
 def fetch_recent_papers(name, num_papers=5):
     try:
-        # Search for the author
         search_query = scholarly.search_author(name)
         author = next(search_query)
         
-        # Fill in author details including publications
         author = scholarly.fill(author, sections=['publications'], sortby='year')
-        
-        # Get publications and sort by year for recent papers
+                
         publications = author['publications']
+        
         sorted_by_year = sorted(publications, key=lambda x: x['bib'].get('pub_year', '0'), reverse=True)
-        
-        # Sort by citations for most cited papers
         sorted_by_citations = sorted(publications, key=lambda x: x.get('num_citations', 0), reverse=True)
-        
-        # Get the papers
+    
         recent_papers = []
         cited_papers = []
         
-        # Process recent papers
-        for pub in sorted_by_year[:num_papers]:
-            filled_pub = scholarly.fill(pub, sections=['bib', 'num_citations'])
-            paper = {
-                'title': filled_pub['bib'].get('title'),
-                'year': filled_pub['bib'].get('pub_year'),
-                'citations': filled_pub.get('num_citations', 0),
-                'abstract': filled_pub['bib'].get('abstract', 'Abstract not available')
-            }
-            recent_papers.append(paper)
+        used_titles = set()
+
+        def add_papers(source_pubs, target_array, num_papers):        
+            def process_paper(pub):
+                filled_pub = scholarly.fill(pub, sections=['bib', 'num_citations'])
+                return {
+                    'title': filled_pub['bib'].get('title'),
+                    'year': filled_pub['bib'].get('pub_year'),
+                    'citations': filled_pub.get('num_citations', 0),
+                    'abstract': filled_pub['bib'].get('abstract', 'Abstract not available')
+                }
             
-        # Process most cited papers
-        for pub in sorted_by_citations[:num_papers]:
-            # Skip if paper is already in recent papers to avoid duplicate API calls
-            if pub['bib'].get('title') in [p['title'] for p in recent_papers]:
-                continue
+            for pub in source_pubs[:num_papers]:
+                if pub['bib'].get('title') in used_titles:
+                    continue
+                paper = process_paper(pub)
+                target_array.append(paper)
+                used_titles.add(paper['title'])
                 
-            filled_pub = scholarly.fill(pub, sections=['bib', 'num_citations'])
-            paper = {
-                'title': filled_pub['bib'].get('title'),
-                'year': filled_pub['bib'].get('pub_year'),
-                'citations': filled_pub.get('num_citations', 0),
-                'abstract': filled_pub['bib'].get('abstract', 'Abstract not available')
-            }
-            cited_papers.append(paper)
+        add_papers(sorted_by_year, recent_papers, num_papers)
+        add_papers(sorted_by_citations, cited_papers, num_papers)
             
         return {
             'recent_papers': recent_papers,
@@ -105,8 +97,8 @@ def fetch_scholar_profile(name):
 
 if __name__ == "__main__":
     print("hello world")
-    professor_name = professors[0]#"Mark Plumbley"
-    papers = fetch_recent_papers(professor_name, 3)
+    professor_name = random.choice(professors)
+    papers = fetch_recent_papers(professor_name, 2)
     print(papers)
     # result = fetch_scholar_profile(professor_name)
     # print(result)
